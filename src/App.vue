016d95f79aa7f74ff69dad0caf7f393448f066b2
tanhong <tanhong@asiainfo.com>
2021-02-23 16:47:57 +0800

    qiankun-base代码提交

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <menu1></menu1>
+    <div>
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import menu1 from "@/components/menu1";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    menu1
   }
 }
 </script>
@@ -21,8 +23,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
